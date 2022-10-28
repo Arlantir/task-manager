8656a62fbe907d8b565a4c8628ece314c88a6d1d
Arlantir <ror@worldoflosttime.ru>
2022-10-29 02:41:42 +0300

    feat: add model user and task with test

---
+++ test/factories/sequence.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+    sequence :string, aliases: [:first_name, :last_name, :password, :name, :state, :type] do |n|
+      "string#{n}"
+    end
+
+    sequence :text, aliases: [:description] do |n|
+      "text#{n}"
+    end
+
+    sequence :email do |n|
+      "person#{n}@example.com"
+    end
+
+		sequence :avatar do |n|
+			"avatar_#{n}.jpg"
+		end
+  end
